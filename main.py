4b73ccd0d88f0ca8f12a5d962c558a1836b5029a
Suryalucky10 <161465734+Suryalucky10@users.noreply.github.com>
2025-07-07 20:36:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,17 +5,3 @@ app=FastAPI()
 
 app.include_router(router)
 
-# from pymongo.mongo_client import MongoClient
-# from pymongo.server_api import ServerApi
-
-# uri = "mongodb+srv://Homelander:987654321@pymongo.w0agsot.mongodb.net/?retryWrites=true&w=majority&appName=PyMongo"
-
-# # Create a new client and connect to the server
-# client = MongoClient(uri, server_api=ServerApi('1'))
-
-# # Send a ping to confirm a successful connection
-# try:
-#     client.admin.command('ping')
-#     print("Pinged your deployment. You successfully connected to MongoDB!")
-# except Exception as e:
-#     print(e)
